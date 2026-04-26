66b4945442814c73c77fe2e55a08f4ce1c6aab37
bonggal <bonggal.siahaan@gmail.com>
2026-04-26 14:38:57 +0700

    Init spring

---
+++ src/test/java/id/colossus/core/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package id.colossus.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
